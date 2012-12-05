fd7de7a9960ac1d20eda5e0b9a74a97293bab3bb
David Würfel <david@devinmotion.de>
2012-12-05 14:25:47 +0100

    run rmiregistry and java in background (to allow complete execution)

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@
 javac */*.java
 jar cvf balls.jar */*.class
 
-rmiregistry
+# with & run in background
+rmiregistry &
 
-java -classpath balls.jar -Djava.security.policy=security.policy balls.Player 2
-java -classpath balls.jar -Djava.security.policy=security.policy balls.Player localhost Bill
-java -classpath balls.jar -Djava.security.policy=security.policy balls.Player localhost Steve
+java -classpath balls.jar -Djava.security.policy=security.policy balls.Player 2 &
+# wait a moment to let BallBoss register
+sleep 5
+java -classpath balls.jar -Djava.security.policy=security.policy balls.Player localhost Bill &
+# wait another moment
+sleep 5
+java -classpath balls.jar -Djava.security.policy=security.policy balls.Player localhost Steve &
+
+# now everything goes to the same console
